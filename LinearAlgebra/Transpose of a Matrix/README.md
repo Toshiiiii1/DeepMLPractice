2a6414c299c4b061f486b72a72d98f1c44f2edbe
Toshi <huynhduykhoi619@gmail.com>
2026-01-06 09:28:48 +0700

    transpose a matrix with zip function

--- LinearAlgebra/Transpose of a Matrix/README.md
+++ LinearAlgebra/Transpose of a Matrix/README.md
@@ -12,3 +12,9 @@ Write a Python function that computes the transpose of a given matrix.
 **Output:**
 
 ```[[1,4],[2,5],[3,6]]```
+
+### Solution:
+
+Cách ban đầu là dùng 2 loop lồng nhau để lấy các phần tử mỗi cột, gom chúng vào một list (một row mới) rồi append vào ma trận mới.
+
+Nhưng có cách hay hơn đó là dùng hàm zip(*a), *a dùng để tách từng row trong ma trận a thành từng tham số truyền vào hàm zip(). Phần còn lại của hàm zip() chỉ là gom các phần tử cùng vị trí ở từng row vào một tuple (một row mới). Biến đổi mỗi tuple thành list rồi tạo list mới gom các row mới là xong.
--- LinearAlgebra/Transpose of a Matrix/solution.py
+++ LinearAlgebra/Transpose of a Matrix/solution.py
@@ -1,24 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 def transpose_matrix(a: list[list[int|float]]) -> list[list[int|float]]:
-	c = []
-    # iterate over the columns of the matrix
-	for i in range(len(a[0])):
-        # iterrate over the rows of the matrix
-        # create new row from the ith column of the original matrix
-		b = [a[j][i] for j in range(len(a))]
-		c.append(b)
-	return c
+    # a = [[a00, a01, a02,... a0n], [a10, a11, a12,... a1n]]
+    # zip(*a) = (a00, a10), (a01, a11), (a02, a12),... (a0n, a1n)
+    # map(): (a00, a10) -> [a00, a10]
+    return list(map(list, zip(*a)))
 
-
-# In[3]:
-
-
-# input
 a = [[1,2,3],[4,5,6]]
 print(transpose_matrix(a))
 
