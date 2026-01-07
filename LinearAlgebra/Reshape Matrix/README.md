d92a681690fcf3e4a47a3ce89a0b32eaa9467cd3
Toshi <huynhduykhoi619@gmail.com>
2026-01-07 08:51:20 +0700

    reshape a matrix with vanilla python

--- LinearAlgebra/Reshape Matrix/README.md
+++ LinearAlgebra/Reshape Matrix/README.md
@@ -12,3 +12,9 @@ Write a Python function that reshapes a given matrix into a specified shape. if 
 **Output:**
 
 ```[[1, 2], [3, 4], [5, 6], [7, 8]]```
+
+### Solution:
+
+Đề bài cho ma trận a và shape mới của ma trận, ở cái shape mới chỉ cần chú ý giá trị columns là ok.
+
+Duyệt qua từng row trong ma trận a, dùng một loop (i=0, i < len(row), i += columns), mỗi row thì dùng slicing từ khoảng i -> i + columns tạo thành row mới, thêm row mới vào ma trận kết quả là xong.
--- LinearAlgebra/Reshape Matrix/solution.py
+++ LinearAlgebra/Reshape Matrix/solution.py
@@ -1,24 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-import numpy as np
-
 def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
-	rows, cols = np.array(a).shape
-    # the total number of elements of the matrix after reshaping must be equal to the total number of elements of the original matrix.
-	if rows * cols != new_shape[0] * new_shape[1]:
-		return []
-	reshaped_matrix = np.array(a).reshape(new_shape).tolist()
-	return reshaped_matrix
+    columns = new_shape[1]
+    if len(a) + len(a[0]) == sum(new_shape):
+        newArray = []
+        for row in a:
+            for i in range(0, len(row), columns):
+                newArray.append(row[i:i+columns])
 
+        return newArray
+    else:
+        return []
 
-# In[3]:
-
-
-# input
 a = [[1,2,3,4],[5,6,7,8]]
 new_shape = (4, 2)
 print(reshape_matrix(a, new_shape))
